ed103831790d24513945900bc14c865942e0fa00
0915318 <0915318@hr.nl>
2018-11-12 14:40:29 +0100

    LAtest
    ewa

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from easygui import *
-msgbox("Danger, Will Robinson!", "kakapoopoo!")
+import sys
+
+msg = "Do you want to continue?"
+title = "Please Confirm"
+if ccbox(msg, title):     # show a Continue/Cancel dialog
+    pass  # user chose Continue
+else:  # user chose Cancel
+    sys.exit(0)
